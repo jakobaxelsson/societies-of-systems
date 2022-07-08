0cd06958c42b2a703092bf1cc0e6a82035f79819
Jakob Axelsson <jakob.pm.axelsson@gmail.com>
2022-07-08 16:30:31 +0200

    Updates

--- build.py
+++ build.py
@@ -12,7 +12,15 @@ from dominate.util import raw
 import mistune
 import yaml
 
-html_parser = mistune.create_markdown()
+class BulmaRenderer(mistune.HTMLRenderer):
+    """
+    A mistunes renderer which extends the HTML renderer to also include Bulma styling information.
+    """    
+#    pass
+    def heading(self, text, level):
+        return f'<h{level} class="title is-{level}">{text}</h{level}>'
+
+html_parser = mistune.create_markdown(renderer = BulmaRenderer())
 
 def process_file(path: Path):
     """
@@ -31,6 +39,14 @@ def process_file(path: Path):
     html = html_parser(body)
     return directives, html
 
+# General CSS directives
+css = """
+img { display: block; margin-left: auto; margin-right: auto; }
+h1 { text-transform: uppercase; text-align: center; }
+a { color: black; text-decoration: underline; }
+* { font-family: "Century Schoolbook", Century, Garamond, serif; }
+"""
+
 def create_page(path: Path, body: str):
     """
     Creates a html page containing the body text provided in the path provided. 
@@ -40,31 +56,39 @@ def create_page(path: Path, body: str):
         body (str): the html text of the body of the page.
     """
     # TODO: Add proper formating.
-    with dominate.document() as d:
-        dom.p(dom.a("Home", href = "/index.html"))
-        dom.p(dom.a("About", href = "/about.html"))
-        dom.p(dom.a("Jakob Axelsson", href = "/jakob-axelsson.html"))
-        dom.p(dom.img(src = "/assets/banner.jpg"))
-        dom.h1("Societies of Systems")
-        dom.h2("A journey into digitalization and the engineering of complex systems")
-        raw(body)
+    with dominate.document(title = "Societies of Systems") as d:
+        # Header information
+        with d.head:
+            dom.meta(name = "viewport", content = "width=device-width, initial-scale=1")
+            dom.link(rel="stylesheet", href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css")
+            dom.link(rel = "icon", type = "image/png", sizes = "32x32", href = "/favicon-bee.png")
+            with dom.style() as s:
+                s += css
+
+        # Navbar
+        with dom.nav(cls = "navbar"):
+            dom.a("Home", href = "/index.html", cls = "navbar-item")
+            dom.a("About", href = "/about.html", cls = "navbar-item")
+            dom.a("Jakob Axelsson", href = "/jakob-axelsson.html", cls = "navbar-item")
+
+        # Banner
+        with dom.div(style = "position: relative; text-align: center; color: white;"):
+            dom.img(src = "/assets/banner.jpg", style = "object-fit: cover; height: 300px; width: 100%; ")
+            dom.h1("Societies of Systems", 
+                   style = "position: absolute; top: 60%; left: 50%; transform: translate(-50%, -50%); text-transform: uppercase;", 
+                   cls = "title is-1 has-text-white")
+            dom.h2("A journey into digitalization and the engineering of complex systems", 
+                   style = "position: absolute; top: 85%; left: 50%; transform: translate(-50%, -50%);", 
+                   cls = "subtitle is-4 has-text-white is-italic")
+
+        # Body
+        with dom.div(cls = "columns is-centered"):
+            with dom.div(cls = "column is-half"):
+                with dom.div(cls = "content"):
+                    raw(body)
     with path.open("w") as f:
         f.write(d.render())
     print(f"Generated {path}")
-
-def change_suffix(path: Path, suffix: str) -> str:
-    """
-    Changes the suffix of the given filename.
-
-    Args:
-        path (Path): the file path, including old suffix.
-        suffix (str): the new suffix.
-
-    Returns:
-        str: the filename with the new suffix.
-    """
-    return path.parent / (path.stem + "." + suffix)
-
 
 if __name__ == "__main__":
     source = Path("source")
